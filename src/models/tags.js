100dee26cdf56181afcdeeb68c676b9472222b8e
Fabio Flores Regueira <fabio.flores.regueira@gmail.com>
2024-10-02 20:35:09 -0300

    Corrigido a instrução SQL

--- src/models/tags.js
+++ src/models/tags.js
@@ -51,15 +51,14 @@ Tags.findById = function (id, result) {
 Tags.findAll = function (result) {
 
   var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
-      xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
-      xSql += 'FROM tags '; 
-      //xSql += 'WHERE status =1 '; 
-      xSql += 'GROUP BY iDTag ';  
-      xSql += 'HAVING DATEDIFF(vigencia, CURDATE()) <= 0 and 30 '; 
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) between 31 and 60 ';
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) between 61 and 90 ';  
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) > 90 '; 
-      xSql += 'ORDER BY Diasvigencia'
+      xSql += 'DATEDIFF(vigencia, CURDATE()) as Diasvigencia ';  
+      xSql += 'FROM tags '  
+      xSql += 'WHERE (DATEDIFF(vigencia, CURDATE()) < 0 ) ';
+      xSql += 'OR (DATEDIFF(vigencia, CURDATE()) BETWEEN 0  AND 30) ';  
+      xSql += 'OR (DATEDIFF(vigencia, CURDATE()) BETWEEN 31 AND 60) '; 
+      xSql += 'OR (DATEDIFF(vigencia, CURDATE()) BETWEEN 61 AND 90) '; 
+      xSql += 'OR DATEDIFF(vigencia, CURDATE()) > 90 ';   
+      xSql += 'ORDER BY Diasvigencia '
 
     dbConn.query(xSql, function (err, res) {
     if(err) {
@@ -80,13 +79,8 @@ Tags.findAllativos = function (result) {
   var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
       xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
       xSql += 'FROM tags '; 
-      xSql += 'WHERE status =1 '; 
-      xSql += 'GROUP BY iDTag ';  
-      xSql += 'HAVING DATEDIFF(vigencia, CURDATE()) <= 0 and 30 '; 
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) between 31 and 60 ';
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) between 61 and 90 ';  
-      xSql += 'OR DATEDIFF(vigencia,CURDATE()) > 90 '; 
-      xSql += 'ORDER BY Diasvigencia'
+      xSql += 'WHERE status =1 ';  
+      xSql += 'ORDER BY Diasvigencia '
 
     dbConn.query(xSql, function (err, res) {
     if(err) {
@@ -103,10 +97,74 @@ Tags.findAllativos = function (result) {
 // Metodo: Lista todas as tags de serviço que estão ativas 0 a 30 dias para o termino de vigencia .
 Tags.find0a30 = function(result){
     var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
-    xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
-    xSql += 'FROM tags ';
-    xSql += 'WHERE status =1 and DATEDIFF(vigencia, CURDATE()) between 0 and 30 ';
-    xSql += 'ORDER BY Diasvigencia'
+        xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
+        xSql += 'FROM tags ';
+        xSql += 'WHERE status = 1 '; 
+        xSql += 'AND ( (DATEDIFF(vigencia, CURDATE()) < 0 ) OR (DATEDIFF(vigencia, CURDATE()) BETWEEN 0 and 30) ) ';
+        xSql += 'ORDER BY Diasvigencia'
+
+    dbConn.query(xSql, function (err, res) {
+    if(err) {
+      console.log("error: ", err);
+      result(null, err);
+    }
+    else{
+      console.log('Tags : ', res);
+      result(null, res);
+    }
+    });
+
+}
+
+// Metodo: Lista todas as tags de serviço que estão ativas 31 a 60 dias para o termino da vigenci.
+Tags.find31a60 = function(result){
+  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
+      xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
+      xSql += 'FROM tags ';
+      xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) between 31 and 60 ';
+      xSql += 'ORDER BY Diasvigencia'
+
+    dbConn.query(xSql, function (err, res) {
+    if(err) {
+      console.log("error: ", err);
+      result(null, err);
+    }
+    else{
+      console.log('Tags : ', res);
+      result(null, res);
+    }
+    });
+
+}
+
+// Metodo: Lista todas as tags de serviço que estão ativas 61 a 90 dias para o termino da vigenci.
+Tags.find61a90 = function(result){
+  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
+      xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
+      xSql += 'FROM tags ';
+      xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) between 61 and 90 ';
+      xSql += 'ORDER BY Diasvigencia'
+
+    dbConn.query(xSql, function (err, res) {
+    if(err) {
+      console.log("error: ", err);
+      result(null, err);
+    }
+    else{
+      console.log('Tags : ', res);
+      result(null, res);
+    }
+    });
+
+}
+
+// Metodo: Lista todas as tags de serviço que estão ativas maior que 90 dias para o termino da vigenci.
+Tags.findmaior90 = function(result){
+  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
+      xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
+      xSql += 'FROM tags ';
+      xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) > 90 ';
+      xSql += 'ORDER BY Diasvigencia'
 
   dbConn.query(xSql, function (err, res) {
   if(err) {
@@ -121,76 +179,13 @@ Tags.find0a30 = function(result){
 
 }
 
-// Metodo: Lista todas as tags de serviço que estão ativas 31 a 60 dias para o termino da vigenci.
-Tags.find31a60 = function(result){
-  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
-  xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
-  xSql += 'FROM tags ';
-  xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) between 31 and 60 ';
-  xSql += 'ORDER BY Diasvigencia'
-
-dbConn.query(xSql, function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}
-else{
-  console.log('Tags : ', res);
-  result(null, res);
-}
-});
-
-}
-
-// Metodo: Lista todas as tags de serviço que estão ativas 61 a 90 dias para o termino da vigenci.
-Tags.find61a90 = function(result){
-  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
-  xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
-  xSql += 'FROM tags ';
-  xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) between 61 and 90 ';
-  xSql += 'ORDER BY Diasvigencia'
-
-dbConn.query(xSql, function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}
-else{
-  console.log('Tags : ', res);
-  result(null, res);
-}
-});
-
-}
-
-// Metodo: Lista todas as tags de serviço que estão ativas maior que 90 dias para o termino da vigenci.
-Tags.findmaior90 = function(result){
-  var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
-  xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
-  xSql += 'FROM tags ';
-  xSql += 'WHERE status = 1 and DATEDIFF(vigencia, CURDATE()) > 90 ';
-  xSql += 'ORDER BY Diasvigencia'
-
-dbConn.query(xSql, function (err, res) {
-if(err) {
-  console.log("error: ", err);
-  result(null, err);
-}
-else{
-  console.log('Tags : ', res);
-  result(null, res);
-}
-});
-
-}
-
 // Metodo: Lista todas as tags de serviço que estão inativa. 
 Tags.findAllinativos = function (result) {
   var xSql = 'SELECT iDTag, descricao, categoria, infor, vigencia, status, created_at, updated_at, ';
       xSql += 'DATEDIFF(vigencia,CURDATE()) as Diasvigencia ';
       xSql += 'FROM tags '; 
       xSql += 'WHERE status=0 '; 
-      xSql += 'ORDER BY Diasvigencia desc'
+      xSql += 'ORDER BY Diasvigencia '
 
     dbConn.query(xSql, function (err, res) {
     if(err) {
@@ -203,7 +198,6 @@ Tags.findAllinativos = function (result) {
     }
   });
 };
-
 
 
 // Metodo: Atualiza uma Tag de serviço por ID.
